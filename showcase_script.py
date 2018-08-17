ac3d6f6deb744e2c9e9b36c6df27aa181cc48eb3
Joan Alabort <joan.alabort@hudl.com>
2018-08-17 11:35:21 +0100

    Update example

--- showcase_script.py
+++ showcase_script.py
@@ -4,7 +4,14 @@ from showcase.module_b import B
 b = B(1, 2)
 a = A(b, 3, c=4, d=5)
 
-print(b.to_config())
-print(a.to_config())
-print(a.method1())
-print(a.method2())
+print(f'`b` config is: {b.to_config()}')
+print(f'`a` config is: {a.to_config()}')
+
+print(f'`a` method1 result is: {a.method1()}')
+print(f'`a` method2 result is: {a.method2()}')
+
+print(f'`A` bases are : {A.__bases__}')
+print(f'`A` mro is : {A.__mro__}')
+
+print(f'`B` bases are : {B.__bases__}')
+print(f'`B` mro is : {B.__mro__}')
